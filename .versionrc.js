ab5c8fe670b184ad12c7bf3dae827a27c9e449ed
rupeshtiwari <roopkt@gmail.com>
2021-02-09 15:50:13 -0500

    chore: bump version number

--- .versionrc.js
+++ .versionrc.js
@@ -2,10 +2,12 @@ const tracker = [
   {
     filename: "src/package.json",
     updater: require("standard-version"),
+    type: "json"
   },
   {
     filename: "package.json",
     updater: require("standard-version"),
+    type: "json"
   },
 ];
 
