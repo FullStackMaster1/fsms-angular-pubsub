ae58c3cdf5f895b1fbf6d87e56f3e5be704af1ac
rupeshtiwari <roopkt@gmail.com>
2021-02-10 08:28:37 -0500

    fix:workflow

--- karma.conf.js
+++ karma.conf.js
@@ -10,17 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    junitReporter: {
+      outputDir: 'coverage', // results will be saved as $outputDir/$browserName.xml
+      useBrowserName: false, // add browser name to report and classes names
+      outputFile: 'test-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/fsms-angular-pubsub-ws'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
