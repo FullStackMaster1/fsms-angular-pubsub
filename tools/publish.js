bbfb470706c631625eeda22c288f39ac0851162b
rupeshtiwari <roopkt@gmail.com>
2021-02-12 18:00:02 -0500

    build: azure

--- tools/publish.js
+++ tools/publish.js
@@ -1,6 +1,5 @@
 const execSync = require('child_process').execSync;
-const app =  process.argv[2],
-
+const app =  process.argv[2];
 try {
   const cwd = path.resolve(__dirname, '../', `dist/apps/${app}`);
   execSync(`npm publish --access public`, { cwd, stdio: [0, 1, 2] });
