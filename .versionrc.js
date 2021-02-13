854a15a9b193f883f7f20df428c84aa61b6c36f7
rupeshtiwari <roopkt@gmail.com>
2021-02-13 10:28:17 -0500

    build:git

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "./projects/fsms-angular-pubsub/package.json",
+      type: "json",
+    },
+    {
+      filename: "./package.json",
+      type: "json",
+    },
+  ],
+};
